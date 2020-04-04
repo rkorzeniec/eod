5843291b645bf5c7d6eb88b12b532d2bf4c47891
Robert Korzeniec <r.korzeniec@gmail.com>
2020-04-04 10:17:46 +0200

    Initial Commit

---
+++ eod/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  eod
+//
+//  Created by Robert Korzeniec on 04.04.20.
+//  Copyright © 2020 Robert Korzeniec. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
